4f82e9db7ee61f3ecceb74ffe6514bcc964cfb20
abdullohalimov <abdullohalimov@yandex.com>
2023-12-13 03:43:04 +0500

    changes

--- namozvaqtlari/namozvaqtlari/settings.py
+++ namozvaqtlari/namozvaqtlari/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'namozvaqtlari.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'dbfile/db.sqlite3',
     }
 }
 
